02df8b970829551a1f905e3da296a1136b1c3199
Ian <ianchipotinluis31@gmail.com>
2026-05-08 08:23:28 -0600

    Procesador comandos

--- practica01/procesador_comandos.py
+++ practica01/procesador_comandos.py
@@ -63,12 +63,15 @@ def iniciar_agente():
             print(obtener_saludo(Nombre_bot))
         elif comando == "ayuda":
             print(mostrar_ayuda())
+        elif comando == "recordar":
+            print(procesar_comando_recordar(argumento))
         elif comando == "salir":
             print("Hasta luego")
             ejecutando = False
+        elif comando == "uptime":
+            print(calcular_uptime(hora_inicio))
         else:
             print("Comando no reconocido")
-
 
 def main():
     iniciar_agente()
